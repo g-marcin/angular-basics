8536591a589f4097d6ff8859592fecfdb2e7bd58
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-31 11:15:33 +0200

    clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular-todo';
-  items: string[] = ['one', 'two', 'three'];
+export class AppComponent implements OnInit, OnDestroy {
   ngOnInit() {
     console.log('component initialized');
     return;
   }
+  title = 'angular-todo';
+  items: string[] = ['one', 'two', 'three'];
   data: any;
   staticFunction() {
     return this.title;
   }
-  
+
+  ngOnDestroy(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
-import { DivComponent } from './div/div.component';
-import { MyInterceptorComponent } from './my-interceptor/my-interceptor.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, ButtonComponent, DivComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MyInterceptorComponent,
-      multi: true,
-    },
-  ],
+  declarations: [AppComponent, ButtonComponent, InputComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
