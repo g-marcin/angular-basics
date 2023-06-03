d29c7ee4e022af83702246acb6b730307d32a36f
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-06-03 18:10:43 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { LocalStorageService } from './services/local-storage.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -26,19 +25,5 @@ export class AppComponent {
     const todoCopy = [...this.todos];
     this.todos = todoCopy.filter((todo) => todo != e);
     console.log(this.todos);
-  }
-  test() {
-    const observable = new Observable((subscriber) => {
-      subscriber.next('Hello');
-      subscriber.next('World');
-      subscriber.complete();
-    });
-    const subscription = observable.subscribe({
-      next: (value) => console.log(value),
-      complete: () => console.log('Observable completed'),
-    });
-
-    // Unsubscribe from the Observable
-    subscription.unsubscribe();
   }
 }
