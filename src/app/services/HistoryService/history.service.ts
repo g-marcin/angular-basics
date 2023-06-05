98029745f2ee55f07e000e388e70cae2d74b78b6
g-marcin <marcin.grzmil@hotmail.com>
2023-06-05 21:39:38 +0200

    typed history service response

--- src/app/services/HistoryService/history.service.ts
+++ src/app/services/HistoryService/history.service.ts
@@ -1,22 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
 import { format, subDays } from 'date-fns';
 
+interface currencyHistoryResponse extends Response{
+  rates:{[date:string]:{[code:string]:number}}
+}
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class HistoryService {
   baseURL = 'https://api.frankfurter.app/';
-
   constructor(private httpClient: HttpClient) {}
 
   getCurrencyHistory(baseCurrency: string) {
-    //TODO:type the history response
     const date = new Date();
     const dateFrom = format(date, 'yyyy-MM-dd');
     const dateTo = format(subDays(date, 12), 'yyyy-MM-dd');
-    return this.httpClient.get<any>(
+    
+    return this.httpClient.get<currencyHistoryResponse>(
       `${this.baseURL}${dateTo}..${dateFrom}?from=${baseCurrency}`
     );
   }
