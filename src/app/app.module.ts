f1809c6f4e1785bb122442c15d1489ecf548022f
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-06-06 22:40:55 +0200

    moved currency-item and history-item to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CountryFlagComponent } from './components/country-flag/country-flag.component';
-import { FooterComponent, HeaderComponent } from './components';
+import {
+  FooterComponent,
+  HeaderComponent,
+  CurrencyItemComponent,
+  CountryFlagComponent,
+  HistoryItemComponent,
+} from './components';
 
 import {
   AdminComponent,
   BaseComponent,
-  CurrencyItemComponent,
   DetailsComponent,
   DisplayComponent,
   ExchangeComponent,
   HistoryComponent,
-  HistoryItemComponent,
   LatestComponent,
   ListComponent,
 } from './pages';
